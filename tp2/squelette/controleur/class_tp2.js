e7b55fd3b2a9096e399026068060f90fcfd3ccc6
humber377u <gabin.humbert9@etu.univ-lorraine.fr>
2024-03-07 13:39:14 +0100

    tp2

---
+++ tp2/squelette/controleur/class_tp2.js
@@ -0,0 +1,60 @@
+class VueTp2 {
+    init(form) {
+        this._form = form;
+        this.form.edtSaisie.onkeydown = function (event) { vueTp2.ajouterSaisie(event); };
+        this.form.chkTri.onkeydown = function () { vueTp2.trierListe(); };
+        this.form.btnVider.onkeydown = function () { vueTp2.viderListe(); };
+        this.form.btnSupprimer.onkeydown = function () { vueTp2.supprimerLigne(); };
+    }
+    get form() { return this._form; }
+    viderListe() {
+        this.form.listeTexte.length = 0;
+    }
+    supprimerLigne() {
+        const liste = this.form.listeTexte;
+        const noLigne = liste.selectedIndex;
+        if (noLigne > -1) {
+            liste.remove(noLigne);
+        }
+    }
+    triListe(liste) {
+        const options = liste.options;
+        let optionsArray = [];
+        for (let i = 0; i < options.length; i++) {
+            optionsArray.push(options[i]);
+        }
+        optionsArray = optionsArray.sort(function (a, b) {
+            if (a.value > b.value) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        });
+        for (let i = 0; i <= options.length; i++) {
+            options[i] = optionsArray[i];
+        }
+    }
+    trierListe() {
+        if (this.form.chkTri.checked) {
+            this.triListe(this.form.listeTexte);
+        }
+    }
+    ajouterSaisie(ev) {
+        if (ev.key === 'Enter') {
+            const elt = this.form.edtSaisie;
+            const liste = this.form.listeTexte;
+            const chaine = elt.value.trim();
+            if (chaine !== "") {
+                const opt = new Option(chaine, chaine);
+                liste.options.add(opt);
+                this.trierListe();
+            }
+            elt.value = "";
+            elt.focus();
+        }
+    }
+}
+let vueTp2 = new VueTp2;
+export { vueTp2 };
+//# sourceMappingURL=class_tp2.js.map
