9780b60a0e2597243a03454e4e9b22cb927354f6
humber377u <gabin.humbert9@etu.univ-lorraine.fr>
2024-03-10 14:30:16 +0000

    correc ?

--- tp2/squelette/controleur/tp2.js
+++ tp2/squelette/controleur/tp2.js
@@ -1,8 +1,8 @@
 import { vueTp2 } from "../controleur/class_tp2";
-vueTp2.init({ edtSaisie: document.querySelector("[id=edt_saisie]"),
-    listeTexte: document.querySelector("[id=select_texte"),
-    chkTri: document.querySelector("[id=chk_tri]"),
-    btnVider: document.querySelector("[id=btn_vider]"),
-    btnSupprimer: document.querySelector("[id=btn_supprimer]"),
+vueTp2.init({ edtSaisie: document.querySelector('[id=edt_saisie]'),
+    listeTexte: document.querySelector('[id=select_texte]'),
+    chkTri: document.querySelector('[id=chk_tri]'),
+    btnVider: document.querySelector('[id=btn_vider]'),
+    btnSupprimer: document.querySelector('[id=btn_supprimer]')
 });
 //# sourceMappingURL=tp2.js.map
